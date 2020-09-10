04c3354d43b8c872f8361ee0b9a34a23e96d985c
Mitchell Bosman <mitchell.m.bosman@gmail.com>
2020-09-10 18:59:37 -0500

    updated for windows

--- makeProject.py
+++ makeProject.py
@@ -1,42 +1,47 @@
 #! /usr/bin/python3
-import sys, os, time, subprocess
+import sys
+import os
+import time
+import subprocess
 from github import Github
 
 token = os.environ['GitHubAuthKey']
+
+
 def CIFolder(project):
     platform = sys.platform
     if 'lin' in platform:
         print("Linux")
         subprocess.call(['mkdir', project])
         os.chdir(project)
-        subprocess.run([f'echo "# {project}" >> README.md'],shell=True)
-        subprocess.run(['git init'],shell=True)
-        subprocess.run(['git add .'],shell=True)
-        subprocess.run(['git commit -m "Initial Commit"'],shell=True)
-        subprocess.run(['git branch -M master'],shell=True)
-        
+        subprocess.run([f'echo "# {project}" >> README.md'], shell=True)
+        subprocess.run(['git init'], shell=True)
+        subprocess.run(['git add .'], shell=True)
+        subprocess.run(['git commit -m "Initial Commit"'], shell=True)
+        subprocess.run(['git branch -M master'], shell=True)
+
     elif 'win' in platform:
         print('Windows')
-        subprocess.call(['mkdir', project])
+        subprocess.run(['mkdir', project], shell=True)
         os.chdir(project)
-        subprocess.run([f'echo "# {project}" >> README.md'],shell=True)
-        subprocess.run(['git init'],shell=True)
-        subprocess.run(['git add .'],shell=True)
-        subprocess.run(['git commit -m "Initial Commit"'],shell=True)
-        subprocess.run(['git branch -M master'],shell=True)
+        with open('README.md', 'w') as f:
+            f.write(f'# {project}')
+        subprocess.run(['git', 'init'])
+        subprocess.run(['git', 'add', '.'])
+        subprocess.run(['git', 'commit', '-m', '"Initial Commit"'])
+        subprocess.run(['git', 'branch', '-M', 'master'])
 
 
 def InitGit(project):
     g = Github(token)
     repo = g.get_user().create_repo(project)
     print(repo.ssh_url)
-    subprocess.run([f'git remote add origin {repo.ssh_url}'], shell=True)
-    subprocess.run([f'git push -u origin master'], shell=True)
+    subprocess.run(['git', 'remote', 'add', 'origin',
+                    repo.ssh_url], shell=True)
+    subprocess.run(['git', 'push', 'origin', 'master'], shell=True)
 
 
 project_name = sys.argv[1]
+# project_name = 'Hello'
 CIFolder(project_name)
 InitGit(project_name)
-
-
-print("\n---------------\nDoneeeee")
